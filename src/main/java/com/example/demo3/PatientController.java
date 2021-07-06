f2203a8cabecd45b62d2d7181bad4352bb92b4b7
IRonny1 <ironny1604@gmail.com>
2021-07-06 14:07:11 +0300

    CORS Upd

--- src/main/java/com/example/demo3/PatientController.java
+++ src/main/java/com/example/demo3/PatientController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @AllArgsConstructor
 public class PatientController {
